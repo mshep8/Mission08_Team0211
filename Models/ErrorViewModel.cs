b59e31f91b374d29d853d966e1649e28fd638bb7
mshep8 <marycatherineshepherd@gmail.com>
2026-02-23 09:21:24 -0700

    Add all project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission08_Team0211.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
